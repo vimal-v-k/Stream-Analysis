78cb193a5de20c5e6872901caae153f0d274cf7a
vimal-v-k <97240882+vimal-v-k@users.noreply.github.com>
2023-04-07 14:17:33 +0530

    Delete accounts directory

--- accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import UserAccounts, StudentDetail, CounsellorDetail, AgencyDetail
-
-# Register your models here.
-
-admin.site.register(UserAccounts)
-admin.site.register(StudentDetail)
-admin.site.register(CounsellorDetail)
-admin.site.register(AgencyDetail)
